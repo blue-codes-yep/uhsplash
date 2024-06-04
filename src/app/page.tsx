4553bda1b265aabad852c7e27f907eb878bd3037
blue-codes-yep <linzblue5@gmail.com>
2024-06-03 21:37:02 -0400

    Autoplay

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 const HomePage = () => {
   return (
-    <div className="w-full max-w-2xl mx-auto">
+    <div className="w-full mx-auto">
       <div className="relative pb-[56.25%] overflow-hidden">
         <iframe
           className="absolute top-0 left-0 w-full h-full"
-          src="https://www.youtube.com/embed/yCgSrluBxus"
+          src="https://www.youtube.com/embed/yCgSrluBxus?autoplay=1&mute=1"
           style={{ border: '0' }}
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
           allowFullScreen
+          
           title="YouTube Video"
         ></iframe>
       </div>
