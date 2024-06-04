a60a94630e477c63aa0dad54e3071f7ab83ec9a1
blue-codes-yep <linzblue5@gmail.com>
2024-06-03 21:18:05 -0400

    getting youtube video to play

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ const HomePage = () => {
       <div className="relative overflow-hidden" style={{ paddingTop: '56.25%' }}>
         <iframe
           className="absolute top-0 left-0 w-full h-full"
-          src="https://www.youtube.com/watch?v=yCgSrluBxus"
+          src="https://www.youtube.com/embed/watch?v=yCgSrluBxus"
           style={{ border: '0' }}
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
@@ -16,3 +16,4 @@ const HomePage = () => {
 };
 
 export default HomePage;
+
